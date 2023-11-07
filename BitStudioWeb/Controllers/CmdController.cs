7b32a08e45896b1e142c1989067a11b767e1f219
bitkanda <somuchllxx@outlook.com>
2023-11-08 01:21:30 +0800

    提交数据库执行接口。

---
+++ BitStudioWeb/Controllers/CmdController.cs
@@ -0,0 +1,47 @@
+﻿using bitkanda.Dal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace BitStudioWeb.Controllers
+{
+    //[ApiController]
+    [Route("api/[controller]")]
+    public class CmdController : Controller
+    {
+        private readonly MysqlDBContext _dbContext;
+
+        public CmdController(MysqlDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //[Authorize]
+   
+        [HttpPost("Exe")]
+        public ActionResult Exe( )
+        { 
+
+            try
+            {
+               
+                using (var streamReader = new StreamReader(HttpContext.Request.BodyReader.AsStream()))
+                { 
+                    string sql = streamReader.ReadToEnd();
+                    var rows = _dbContext.Database.ExecuteSqlRaw(sql);
+                    return Json(new { rows = rows });
+ 
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error executing SQL: " + ex.Message);
+            }
+
+        }
+    }
+}
--- BitStudioWeb/ReadMe.txt
+++ BitStudioWeb/ReadMe.txt
@@ -1,7 +1,10 @@
 ﻿  知之乐官网
   迁移命令
-  先执行切换到ef的项目文件下，执行
+  先用cmd,执行切换到ef的项目文件下，执行
   cd BitStudioWeb
   再执行：
   dotnet ef migrations add InitialCreateXXXX
   dotnet ef database update
+  
+  执行生成sql命令
+  dotnet ef migrations script -o sql.txt
