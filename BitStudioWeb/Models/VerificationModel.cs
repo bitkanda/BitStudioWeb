95d32d4e8f815a478c933ec5808cee1bdf0b381f
bitkanda <somuchllxx@outlook.com>
2023-11-09 02:21:36 +0800

    添加短信登陆。

---
+++ BitStudioWeb/Models/VerificationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BitStudioWeb.Models
+{
+    public class VerificationModel
+    {
+        [Display(Name ="手机号")]
+        [Required(ErrorMessage = "手机号码是必填的")]
+        [RegularExpression(@"^1[3456789]\d{9}$", ErrorMessage = "请输入有效的手机号码")]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// 验证码
+        /// </summary>
+        [Display(Name = "验证码")]
+        [Required(ErrorMessage = "验证码是必填的")]
+        public int Code { get; set; }
+    }
+}
